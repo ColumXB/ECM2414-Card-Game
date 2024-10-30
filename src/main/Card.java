06d068d2941096f38e0710dbc6b40730aef3dd3f
ColumXB <columxb@gmail.com>
2024-10-30 13:51:22 +0000

    Cards now have weighting

--- src/main/Card.java
+++ src/main/Card.java
@@ -2,6 +2,11 @@ package main;
 public class Card {
 
     private int cardValue;
+    private int weighting = 0;
+    private final int weightingMultiplier = 2;
+    // Base weighting for when a card is a preferred or not preferred by player
+    private final int preferredWeighting = 0;
+    private final int nonPreferredWeighting = 1;
 
 
     /**
@@ -10,6 +15,36 @@ public class Card {
      */
     public int getCardValue() {
         return cardValue;
+    }
+
+
+    /**
+     * Getter for probability weighting of card
+     * @return Probability weighting of card
+     */
+    public int getWeighting() {
+        return weighting;
+    }
+
+
+    /**
+     * Sets probability weight of card depending on if it is preffered or not
+     * @param isPreferred Whether the card is preferred by the player or not
+     */
+    public void setWeighting(boolean isPreferred) {
+        if  (isPreferred) {
+            this.weighting = this.preferredWeighting;
+        } else {
+            this.weighting = this.nonPreferredWeighting;
+        }
+    }
+
+
+    /**
+     * Increases weighting based on preset multiplier
+     */
+    public void incrementWeighting() {
+        this.weighting *= weightingMultiplier;
     }
 
 
