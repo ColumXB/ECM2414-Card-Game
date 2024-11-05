72cb92133e2f544e6a0e429e1bbae53827a40b09
ColumXB <columxb@gmail.com>
2024-11-05 14:57:17 +0000

    Tests for Card (Card_Test.java) are now functional. Card.java passes all the tests.

--- src/test/Card_Test.java
+++ src/test/Card_Test.java
@@ -3,31 +3,40 @@ package test;
 import main.Card;
 import main.InvalidCardException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class Card_Test {
-    
-    int[] passingTestCases = {1, 2, 3, 4, 999999999};
-    int[] failingTestCases = {0, -12, -999999999};
+
     Card card;
 
-
-    // TO DO: FIX
-    @Test  
-    @DisplayName("Test")
-    void testTest() {
-        for (int testCase: failingTestCases) {
-            Throwable exception = assertThrows(InvalidCardException.class, () -> new Card(testCase));
-            assertEquals(String.format("Card value must be a positive integer. Card Value: ", testCase), exception.getMessage());
-        }
-        for (int testCase: passingTestCases) {
-            assertThrows(null, () -> new Card(testCase));
-        }
+    @BeforeEach
+    void refresh() {
+        card = null;
     }
 
+
+    @DisplayName("Valid Values")
+    @ParameterizedTest  
+    @ValueSource(ints = {1, 2, 3, 4, Integer.MAX_VALUE})
+    void validValuesTest(int testCase) {
+
+        assertDoesNotThrow(() -> new Card(testCase));
+    }
+
+
+    @DisplayName("Invalid Values")
+    @ParameterizedTest  
+    @ValueSource(ints = {0, -1, -2, -12, Integer.MIN_VALUE})
+    void invalidValuesTest(int testCase) {
+        assertThrows(InvalidCardException.class, () -> new Card(testCase));
+    }
 }
 
