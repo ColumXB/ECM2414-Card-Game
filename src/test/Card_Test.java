8f9f7073ed8ab0bf926910358141a58f04d2bf94
KadeemHannan200 <161332080+KadeemHannan200@users.noreply.github.com>
2024-11-27 16:43:19 +0000

    Update Card_Test.java

--- src/test/Card_Test.java
+++ src/test/Card_Test.java
@@ -5,8 +5,9 @@ import main.InvalidCardException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -39,5 +40,51 @@ class Card_Test {
         
         assertThrows(InvalidCardException.class, () -> new Card(testCase));
     }
-}
 
+    /*
+     * This test requires a setter for setting example value e.g. card value = 1
+     * Future tests will also need setters
+     */
+    @DisplayName("Set Preferred Weighting")
+    @Test
+    void setPreferredWeightingTest() throws InvalidCardException {
+        Card card = new Card(1); //example card value
+        card.setWeighting(true); // isPreferred is true, so weighting should be set to preferredWeighting (0)
+
+        assertEquals(0, card.getWeighting()); //Weighting should be set to the preferred value (0) when isPreferred is true
+    }
+
+    @DisplayName("Set Non-Preferred Weighting")
+    @Test
+    void setNonPreferredWeightingTest() throws InvalidCardException {
+        Card card = new Card(1); //example card value
+        card.setWeighting(false); // isPreferred is false, so weighting should be set to nonPreferredWeighting (1)
+
+        assertEquals(1, card.getWeighting()); //Weighting should be set to the non-preferred value (1) when isPreferred is false
+    }
+
+
+    @DisplayName("Non-Preferred Weighting Multiplier")
+    @Test
+    void incrementNonPreferredWeightingTest() throws InvalidCardException {
+        int expectedValue = 2;
+        Card card = new Card(10);
+        card.setWeighting(false);
+        card.incrementWeighting();
+        int weighting = card.getWeighting();
+
+        assertEquals(expectedValue, weighting);
+    }
+
+    @DisplayName("Preferred Weighting Multiplier")
+    @Test
+    void incrementPreferredWeightingTest() throws InvalidCardException {
+        int expectedValue = 0;
+        Card card = new Card(10);
+        card.setWeighting(true);
+        card.incrementWeighting();
+        int weighting = card.getWeighting();
+
+        assertEquals(expectedValue, weighting);
+    }
+}
